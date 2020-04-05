cda0f12f7ca5a0ece1748addc57d608122e25b9c
514515479 <514515479@qq.com>
2020-04-05 17:19:10 +0800

    添加consumer模块;

---
+++ cloud-consumer-order80/src/main/java/com/zero/springcloud/ConsumerOrder80.java
@@ -0,0 +1,15 @@
+package com.zero.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: tobi
+ * @Date: 2020/4/5 17:12
+ **/
+@SpringBootApplication
+public class ConsumerOrder80 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerOrder80.class, args);
+    }
+}
