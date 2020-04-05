60f89da07940c647680634eaf8953cec81135923
514515479 <514515479@qq.com>
2020-04-06 00:18:04 +0800

    payment和consumerOrder注册到eureka;

--- cloud-consumer-order80/src/main/java/com/zero/springcloud/ConsumerOrder80.java
+++ cloud-consumer-order80/src/main/java/com/zero/springcloud/ConsumerOrder80.java
@@ -2,12 +2,14 @@ package com.zero.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: tobi
  * @Date: 2020/4/5 17:12
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerOrder80 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerOrder80.class, args);
--- cloud-provider-payment8001/src/main/java/com/zero/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zero/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.zero.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: tobi
  * @Date: 2020/4/4 22:14
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
