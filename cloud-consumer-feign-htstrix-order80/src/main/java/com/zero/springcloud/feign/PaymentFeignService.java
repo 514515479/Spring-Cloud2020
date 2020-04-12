a5d23426020e00dfeb48912aceec1508ccb1387b
514515479 <514515479@qq.com>
2020-04-12 21:13:20 +0800

    hystrix通配服务降级 FeignClient fallback;

--- cloud-consumer-feign-htstrix-order80/src/main/java/com/zero/springcloud/feign/PaymentFeignService.java
+++ cloud-consumer-feign-htstrix-order80/src/main/java/com/zero/springcloud/feign/PaymentFeignService.java
@@ -1,6 +1,7 @@
 package com.zero.springcloud.feign;
 
 import com.zero.common.entity.Result;
+import com.zero.springcloud.feign.fallback.PaymentFeignServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @Author: tobi
  * @Date: 2020/4/11 21:03
  **/
-@FeignClient("CLOUD-PAYMENT-SERVICE")
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE", fallback = PaymentFeignServiceImpl.class)
 public interface PaymentFeignService {
 
     @GetMapping("/payment/hystrix1")
---
+++ cloud-consumer-feign-htstrix-order80/src/main/java/com/zero/springcloud/feign/fallback/PaymentFeignServiceImpl.java
@@ -0,0 +1,22 @@
+package com.zero.springcloud.feign.fallback;
+
+import com.zero.common.entity.Result;
+import com.zero.springcloud.feign.PaymentFeignService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: tobi
+ * @Date: 2020/4/12 21:04
+ **/
+@Component
+public class PaymentFeignServiceImpl implements PaymentFeignService {
+    @Override
+    public Result hystrix1() {
+        return Result.success("PaymentFeignService---fallback");
+    }
+
+    @Override
+    public Result hystrix2() {
+        return Result.success("PaymentFeignService---fallback");
+    }
+}
