4d3f7212f357c7b6f1e4f7044fe9c6d98faf067e
514515479 <514515479@qq.com>
2020-05-03 00:07:47 +0800

    config-3355控制器添加@RefreshScope注解，自动刷新git配置文件的值（有本地缓存）;

--- cloud-config-client-3355/src/main/java/com/zero/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/zero/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.zero.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/configClient")
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
